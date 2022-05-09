bc959e69f1f91f6359cc140429d1cf7fd70dfc45
Viktor Dahlberg <hackzmack@gmail.com>
2022-05-10 00:04:45 +0200

    Init eq commit

---
+++ Customivisualizer/EquipmentManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customivisualizer
+{
+	public class EquipmentManager
+	{
+		private static Lumina.Excel.ExcelSheet<Lumina.Excel.GeneratedSheets.Item>? sheet;
+
+		
+
+		public static void Init(Lumina.Excel.ExcelSheet<Lumina.Excel.GeneratedSheets.Item>? sheet)
+		{
+			if (sheet == null) return;
+			EquipmentManager.sheet = sheet;
+			Build(EquipmentManager.sheet);
+		}
+
+		private static void Build(Lumina.Excel.ExcelSheet<Lumina.Excel.GeneratedSheets.Item> sheet)
+		{
+			foreach (var e in sheet)
+			{
+				//e.ModelMain
+			}
+		}
+	}
+}
